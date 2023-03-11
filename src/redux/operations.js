8aff3d8c30097b8afbf2c17b757079a9c5c0698f
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-03-11 16:31:54 +0200

    fix baseUrl

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// axios.defaults.baseURL = 'https://db-cards.onrender.com';
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://db-cards.onrender.com';
+// axios.defaults.baseURL = 'http://localhost:3000';
 
 export const fetchCards = createAsyncThunk(
   'cards/fetchAll',
