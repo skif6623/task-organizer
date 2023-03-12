333bca68a2dbceef8e9f8e8fa9971f02284ec39d
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-03-12 22:26:24 +0200

    fix address

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://db-cards.onrender.com';
+axios.defaults.baseURL = 'https://task-organizer.onrender.com';
 
 export const fetchCards = createAsyncThunk(
   'cards/fetchAll',
